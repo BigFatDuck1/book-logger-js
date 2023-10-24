a45f7b4cc38679262671045873b8aa635d60519b
BigFatDuck1 <clementho999@gmail.com>
2023-10-24 16:47:54 +0800

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # book-logger-js
+
 Simple site that logs what books you've read
+
+[Link here](https://bigfatduck1.github.io/book-logger-js/)
---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+//All books are stored in this array
+const myLibrary = [];
+
+//Constructor function
+function Book(title, author, pages, read) {
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  //Want, Reading, Completed
+  this.read = read
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
