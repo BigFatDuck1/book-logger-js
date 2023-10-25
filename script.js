c3e734f34a61f11f2ea359e9bc5976d87c95f1e9
BigFatDuck1 <clementho999@gmail.com>
2023-10-25 10:26:40 +0800

    Implemented a function that adds div to the book_cards container

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+
+//1. Book information handling
+
 //All books are stored in this array
 const myLibrary = [];
 
@@ -11,6 +14,7 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
+//Appends book details to array
 function addBookToLibrary(title, author, pages, read) {
 
     let output = new Book(title, author, pages, read);
@@ -23,3 +27,20 @@ function addBookToLibrary(title, author, pages, read) {
 
 console.log(addBookToLibrary("For Whom The Bells Toll", "Ernest Hemingway", 100,"read"))
 console.log(myLibrary)
+
+//2. Turning information into card and adding it to page
+
+let book_cards_container = document.querySelector(".book_cards");
+
+//This function creates a card given the book details
+let addCardFromArray = (book_details) => {
+  //Create div
+  let new_card = document.createElement("div");
+  new_card.textContent = "Hello world!";
+  //Append div to container
+  book_cards_container.appendChild(new_card);
+}
+
+for (let i = 0; i < 20; i++) {
+  addCardFromArray();
+}
